2160b408b73566ac34efbae8425db3dac540cdd3
ihoriosha <ihoriosha@ihoriosha.local>
2018-10-28 21:11:54 +0200

    add new User models, add dev_settings, add managers.py, edit user.py(serializer)

--- settings/settings.py
+++ settings/settings.py
@@ -122,6 +122,15 @@ REST_FRAMEWORK = {
     )
 }
 
+AUTH_USER_MODEL = 'store.User'
+
+# email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
