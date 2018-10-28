6c5af35a357ea1a783927e888664bc2dd13fd60c
taptione <taptione@gmail.com>
2018-10-28 21:54:36 +0200

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,13 +1,1 @@
-from django.contrib.auth import get_user_model
-from rest_framework import viewsets
-from store.api.v1.serializers import UserSerializer
 
-User = get_user_model()
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
