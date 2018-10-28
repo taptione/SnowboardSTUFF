2ade86a147a849391c56724ce2f372a40438b8bd
ihoriosha <ihoriosha@ihoriosha.local>
2018-10-28 21:11:54 +0200

    add new User models, add dev_settings, add managers.py, edit user.py(serializer)

--- store/api/v1/user.py
+++ store/api/v1/user.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers, viewsets
 from rest_framework.authentication import BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
+# from django.core.mail import send_mail
+# from settings.dev_settings import EMAIL_HOST_USER
+from rest_framework.exceptions import APIException
+
+User = get_user_model()
 
 
 class BearerToken(TokenAuthentication):
@@ -11,10 +16,31 @@ class BearerToken(TokenAuthentication):
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'url', 'username', 'email', 'is_staff')
+        fields = ('id', 'email', 'first_name', 'phone', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        if not email or email == 'neo@gmail.com':
+            raise APIException("this is error, Johny")
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data['password']
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        # success register mail
+        # subject = 'snowboard staff successful registration'
+        # message = 'Than`x 4 register {}'.format(user.first_name)
+        # send_mail(subject, message, EMAIL_HOST_USER, [user.email])
+        return user
+
 
 class UserViewSet(viewsets.ModelViewSet):
-    authentication_classes = (BearerToken, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
+    # authentication_classes = (BearerToken, BasicAuthentication)
+    # permission_classes = (IsAuthenticated,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
